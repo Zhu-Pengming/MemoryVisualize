927e7b34b42d4a02acd7f187a03b4e8462915bc7
xyl-lyx1 <150509957+xyl-lyx1@users.noreply.github.com>
2023-11-26 15:29:35 +0800

    Update MemoryManager.java

--- MemoryManagementVisual/src/com/memory/MemoryManager.java
+++ MemoryManagementVisual/src/com/memory/MemoryManager.java
@@ -1,8 +1,9 @@
-package com.memory;
+package MemoryManagementApp;
 
 
 import java.util.ArrayList;
 import java.util.List;
+import MemoryManagementApp.Partition;
 
 public class MemoryManager {
 
@@ -37,31 +38,68 @@ public class MemoryManager {
         }
     }
 
-    public static void firstFit() {
-        Partition partition = new Partition(0, 0);
-        partition.firstFit(MemoryManagementApp.pcbs, MemoryManagementApp.partitions);
+    
+    public static Partition firstFit(PCB[] pcbs, Partition[] partitions, int currentPCBIndex) {
+        // Create an instance of Partition with default values (you may adjust the parameters accordingly)
+        Partition partitionInstance = new Partition(0, 0.0);
+        
+        // Call the non-static method on the instance
+        Partition partition = partitionInstance.firstFit(pcbs, partitions, currentPCBIndex);
+        
+        if (partition != null) {
+            partition.blockId += partition.getSize();
+            partition.partitionSize -= pcbs[currentPCBIndex].getPidSize();
+            if (partition.partitionSize <= MemoryManager.MIN) {
+                partition.partitionSize = 0;
+            }
+        }
         notifyMemoryAllocated();
         display();
+        return partition;
     }
 
-    public static void nextFit() {
-        Partition partition = new Partition(0, 0);
-        partition.nextFit(MemoryManagementApp.pcbs, MemoryManagementApp.partitions);
+    public static Partition nextFit(PCB[] pcbs, Partition[] partitions, int currentPCBIndex) {
+        Partition partition = Partition.nextFit(pcbs, partitions, currentPCBIndex);
+        if (partition != null) {
+            partition.blockId += partition.getSize();
+            partition.partitionSize -= pcbs[currentPCBIndex].getPidSize();
+            if (partition.partitionSize <= MemoryManager.MIN) {
+                partition.partitionSize = 0;
+            }
+        }
         notifyMemoryAllocated();
         display();
+        return partition;
     }
 
-    public static void bestFit() {
-        Partition partition = new Partition(0, 0);
-        partition.bestFit(MemoryManagementApp.pcbs, MemoryManagementApp.partitions);
+    public static Partition bestFit(PCB[] pcbs, Partition[] partitions, int currentPCBIndex) {
+        Partition partition = Partition.bestFit(pcbs, partitions, currentPCBIndex);
+        if (partition != null) {
+            partition.blockId += partition.getSize();
+            partition.partitionSize -= pcbs[currentPCBIndex].getPidSize();
+            if (partition.partitionSize <= MemoryManager.MIN) {
+                partition.partitionSize = 0;
+            }
+        }
         notifyMemoryAllocated();
         display();
+        return partition;
     }
 
-    public static void worstFit() {
-        Partition partition = new Partition(0, 0);
-        partition.worstFit(MemoryManagementApp.pcbs, MemoryManagementApp.partitions);
+    public static Partition worstFit(PCB[] pcbs, Partition[] partitions, int currentPCBIndex) {
+        Partition partition = Partition.worstFit(pcbs, partitions, currentPCBIndex);
+        if (partition != null) {
+            partition.blockId += partition.getSize();
+            partition.partitionSize -= pcbs[currentPCBIndex].getPidSize();
+            if (partition.partitionSize <= MemoryManager.MIN) {
+                partition.partitionSize = 0;
+            }
+        }
         notifyMemoryAllocated();
         display();
+        return partition;
     }
+
+    
+    
 }
