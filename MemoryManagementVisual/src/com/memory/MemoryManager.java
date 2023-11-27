b47561853e07aba2d17c8a8a527fa9ab0277f01a
Tom <89885722+Zhu-Pengming@users.noreply.github.com>
2023-11-27 08:36:29 +0800

    Add files via upload

---
+++ MemoryManagementVisual/src/com/memory/MemoryManager.java
@@ -0,0 +1,55 @@
+package com.memory;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemoryManager {
+
+    static Partition[] partitions;
+    static PCB[] pcbs;
+    static int MIN;
+    private static List<MemoryAllocationListener> listeners = new ArrayList<>();
+
+
+
+    public static void addMemoryAllocationListener(MemoryAllocationListener listener) {
+        listeners.add(listener);
+    }
+
+    public static void notifyMemoryAllocated() {
+        for (MemoryAllocationListener listener : listeners) {
+            listener.memoryAllocated();
+        }
+    }
+
+
+    public static Partition[] firstFit(PCB[] pcbs, Partition[] partitions, int currentPCBIndex) {
+        Partition[] partition = Partition.firstFit(pcbs, partitions, currentPCBIndex);
+
+        notifyMemoryAllocated();
+        return partition;
+    }
+
+
+    public static Partition[] nextFit(PCB[] pcbs, Partition[] partitions, int currentPCBIndex, int lastAllocatedIndex) {
+        Partition[] partition = Partition.nextFit(pcbs, partitions, currentPCBIndex,lastAllocatedIndex);
+        notifyMemoryAllocated();
+        return partition;
+    }
+
+    public static Partition[] bestFit(PCB[] pcbs, Partition[] partitions, int currentPCBIndex) {
+        Partition[] partition = Partition.bestFit(pcbs, partitions, currentPCBIndex);
+        notifyMemoryAllocated();
+        return partition;
+    }
+
+    public static Partition[] worstFit(PCB[] pcbs, Partition[] partitions, int currentPCBIndex) {
+        Partition[] partition = Partition.worstFit(pcbs, partitions, currentPCBIndex);
+        notifyMemoryAllocated();
+        return partition;
+    }
+
+
+
+}
