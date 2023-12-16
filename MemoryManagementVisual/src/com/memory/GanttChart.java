4a0921f07d0dfdea3f4e9a25110c5a7057888460
Tom <89885722+Zhu-Pengming@users.noreply.github.com>
2023-12-16 18:25:11 +0800

    Update GanttChart.java

--- MemoryManagementVisual/src/com/memory/GanttChart.java
+++ MemoryManagementVisual/src/com/memory/GanttChart.java
@@ -1,20 +1,17 @@
 package com.memory;
 
-package MemoryManagement;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 import javafx.collections.FXCollections;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class GanttChart {
 
@@ -24,61 +21,34 @@ public class GanttChart {
     private Set<String> categories; // Maintain a set of unique categories
     private List<String> processOrder;
 
-    public GanttChart() {
+    public GanttChart(String[] processNames) {
         NumberAxis xAxis = new NumberAxis();
-        yAxis = new CategoryAxis();  
+        yAxis = new CategoryAxis();
         xAxis.setAutoRanging(false);
         xAxis.setTickUnit(1);
         chart = new BarChart<>(xAxis, yAxis);
         chart.setTitle("Gantt Chart");
-
         seriesMap = new HashMap<>();
-        categories = new LinkedHashSet<>();
-        processOrder = new ArrayList<>();
-
-        
-    }
-
-
-    public BarChart<Number, String> getChart() {
-        return chart;
+        categories = FXCollections.observableSet(processNames);
+        processOrder = new ArrayList<>(categories);
+        yAxis.setCategories(FXCollections.observableArrayList(processOrder));
     }
 
     public void updateChart(String processName, int startTime, int size) {
         int endTime = startTime + size;
-    
-        // Add the category only if it's not in the set
-        if (!categories.contains(processName)) {
-            categories.add(processName);
-            processOrder.add(processName); // Add the process to the order list
-            yAxis.setCategories(FXCollections.observableArrayList(processOrder));
-        }
-    
+
         XYChart.Series<Number, String> series = seriesMap.computeIfAbsent(processName, k -> {
             XYChart.Series<Number, String> newSeries = new XYChart.Series<>();
             chart.getData().add(newSeries);
             return newSeries;
         });
-    
-        // Use the end time as the x-coordinate, and set the correct duration
-        series.getData().add(new XYChart.Data<>(endTime, processName, new CustomData(size)));
-    
-        System.out.println("Updating Chart: Process " + processName + " - Start: " + startTime + ", End: " + endTime);
+        series.getData().add(new XYChart.Data<>(endTime, processName, size));
+        System.out.println("Updating Chart: Process " + processName +
+                " - Start: " + startTime +
+                ", End: " + endTime);
     }
 
-    
-    
-    
-    
-    public static class CustomData {
-        private double duration;
-
-        public CustomData(double duration) {
-            this.duration = duration;
-        }
-
-        public double getDuration() {
-            return duration;
-        }
+    public BarChart<Number, String> getChart() {
+        return chart;
     }
 }
